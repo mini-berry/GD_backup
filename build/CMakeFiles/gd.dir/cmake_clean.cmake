1dc4f78c3cbc1030d4990b5e4913c6619c76aa7e
lane <exsg@outlook.com>
2023-06-23 12:59:53 +0800

    v230623

---
+++ build/CMakeFiles/gd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/gd"
+  "../bin/gd.pdb"
+  "CMakeFiles/gd.dir/src/main.cpp.o"
+  "CMakeFiles/gd.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
